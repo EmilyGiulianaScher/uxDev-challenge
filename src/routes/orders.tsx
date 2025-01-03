ea6de52716861d2bf54d2db62d4cc5e6496f5767
giuliana scher <emilygiulianascher@gmail.com>
2025-01-03 15:31:23 -0300

    header finalizado, inicio de la seccion table y colores

---
+++ src/routes/orders.tsx
@@ -0,0 +1,10 @@
+export const Orders = () => (
+  <>
+    <h1>Orders</h1>
+    <div>
+      <img src="/public/whitepaper.png" alt="white-paper-image" />
+      <h2>No orders yet</h2>
+      <p>There’s currently no orders placed </p>
+    </div>
+  </>
+);
